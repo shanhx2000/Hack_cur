78eded34fab0a25ee65139b5545244fdfbf55599
Nozidoali <33287308+Nozidoali@users.noreply.github.com>
2019-07-14 00:06:06 +0800

    merge from master to why (#6)
    * change css
    
    * add inside.html
    
    * delete database
    
    * change the style
    
    * put  the app on to the public net
    
    * solve tags.html problem
    
    * solve over write bug
    
    * solve add bug
    
    * solve bug

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def index():
 @app.route('/<tag>')
 def loadtag(tag):
     mydata.add_auto('CYJ is fake, CYJ is strong, CYJ is human, CYJ sits besides me.')
-    return render_template('inside.html',tag_list=mydata.taglist, tag=tag, data_list=mydata.Find(tag),length=len(mydata.Find(tag)))
+
+    return render_template('tags.html',tag_list=mydata.taglist, tag=tag, data_list=mydata.Find(tag),length=len(mydata.Find(tag)))
 
 if(__name__=='__main__'):
     app.run(host='0.0.0.0')
