6cca5c9dec0aa7a76409f2bebd46c5bea7943f70
coned <coned@outlook.com>
2019-07-13 14:27:59 +0800

    change the templete

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template
-import pymongo
+#import pymongo
 
-myclient = pymongo.MongoClient('mongoodb://localhost:27017/')
-mydb = myclient['db']
-mycol = mydb['name']
+#myclient = pymongo.MongoClient('mongoodb://localhost:27017/')
+#mydb = myclient['db']
+#mycol = mydb['name']
 
-mydict = {'name':'run','alexa':'1000','url':'http://www.runoob.com'}
+#mydict = {'name':'run','alexa':'1000','url':'http://www.runoob.com'}
 
-x = mycol.insert_one(mydict)
+#x = mycol.insert_one(mydict)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tag_lists=["23","32","$243"])
 
 #if(__name__=='__main__'):
 #    app.run()
