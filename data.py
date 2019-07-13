b81895642d73f3372c1d5c5e91d579c2e82a0eaf
Nozidoali <whynull@sjtu.edu.cn>
2019-07-13 22:24:40 +0800

    add length in template

--- data.py
+++ data.py
@@ -27,7 +27,6 @@ class profile:
         for tag in dict.tags:
             self.taglist.append(tag)
 
-
     def Find(self,tagname):
         ret = []
         for x in (self.file).find({'tags':tagname}):
